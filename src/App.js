56dc1cbe1613d7bead9ca4a486bb2df626492a6c
haliljon <juraboevhaliljon@gmail.com>
2023-11-05 00:08:34 +0500

    Add logged out functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 function App() {
@@ -9,10 +10,32 @@ function App() {
     setLoggedInStatus("LOGGED_IN")
     setUser(data.user)
   }
+  const checkLoginStatus = () => {
+    axios.get("http://localhost:3001/logged_in", { withCredentials: true }).then(response => {
+      if (response.data.logged_in && loggedInStatus === "NOT_LOGGED_IN") {
+        setLoggedInStatus("LOGGED_IN")
+        setUser(response.data.user)
+      } else if (!response.data.logged_in && loggedInStatus === "LOGGED_IN") {
+        setLoggedInStatus("NOT_LOGGED_IN")
+        setUser({})
+      }
+    }).catch(error => {
+      console.log("check login error", error);
+    });
+  }
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  const handleLogout = () => {
+    setLoggedInStatus("NOT_LOGGED_IN")
+    setUser({})
+  }
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home loggedInStatus={loggedInStatus} handleLogin={handleLogin} />} />
+        <Route path="/" element={<Home loggedInStatus={loggedInStatus} handleLogin={handleLogin} handleLogout={handleLogout} />} />
         <Route path="/dashboard" element={<Dashboard loggedInStatus={loggedInStatus} />} />
       </Routes>
     </div>
