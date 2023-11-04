931de48424cdd61c3a04ea0bf9b5b31a21a20d94
haliljon <juraboevhaliljon@gmail.com>
2023-11-04 18:38:21 +0500

    Add react registration component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
+import Registration from "./auth/Registration";
+
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
             <p>Welcome to the Home page!</p>
+            <Registration />
         </div>
     );
 };
